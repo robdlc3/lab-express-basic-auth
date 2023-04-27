6ff32bf4d8da9cedff60d0764bf057fd0304f461
Robert De La Cruz <rdlc976@gmail.com>
2023-04-27 17:20:46 -0400

    login and signup

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+
+const bcrypt = require('bcryptjs');
+
+const user = require('../models/User.model');
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/login', (req, res, next) => {
+  res.render('login.hbs')
+})
+
+router.get('/signup', (req, res, next) => {
+  res.render('signup.hbs')
+})
 
 module.exports = router;
